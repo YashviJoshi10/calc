85dbfc7b92bfcc7ea534b920feff3d5c06a54b8c
Yashvi <yashvi1095@gmail.com>
2025-10-25 15:05:04 +0530

    changes made

--- script.js
+++ script.js
@@ -13,13 +13,25 @@ const get_nums = (o)=>{
     right_num = ""
     left_idx = n.indexOf(o)-1
     right_idx = n.indexOf(o)+1
-    while (!op.includes(n[left_idx]) && left_idx>=0) {
-        left_num=n[left_idx]+left_num
-        left_idx=left_idx-1
+    if (left_idx==-1) {
+        left_num=0
     }
-    while (!op.includes(n[right_idx]) && right_idx<n.length) {
-        right_num=right_num+n[right_idx]
-        right_idx=right_idx+1
+    else{
+
+        while (!op.includes(n[left_idx]) && left_idx>=0) {
+            left_num=n[left_idx]+left_num
+            left_idx=left_idx-1
+        }
+    }
+    if (right_idx==n.length) {
+        right_num=left_num
+    }
+    else{
+
+        while (!op.includes(n[right_idx]) && right_idx<n.length) {
+            right_num=right_num+n[right_idx]
+            right_idx=right_idx+1
+        }
     }
     // console.log(left_num)
     // console.log(right_num)
@@ -39,14 +51,15 @@ const calc = (n)=>{
         a = parseInt(left_num) * parseInt(right_num)
         n.splice(left_idx+1,right_idx-left_idx-1,a)
         calc(n) 
-    }else if(n.includes("+")){
-        get_nums("+")
-        a = parseInt(left_num) + parseInt(right_num)
-        n.splice(left_idx+1,right_idx-left_idx-1,a)
-        calc(n) 
     }else if(n.includes("-")){
         get_nums("-")
         a = parseInt(left_num) - parseInt(right_num)
+        n.splice(left_idx+1,right_idx-left_idx-1,a)
+        calc(n) 
+    }
+    else if(n.includes("+")){
+        get_nums("+")
+        a = parseInt(left_num) + parseInt(right_num)
         n.splice(left_idx+1,right_idx-left_idx-1,a)
         calc(n) 
     }
@@ -56,13 +69,20 @@ btn.forEach((b)=>{
     b.addEventListener("click",function(){
         // console.log(b.value)
         if(b.value!="AC" && b.value!="="){            
-            if(op.includes(b.value) && op.includes(n[(n.length)-1])){
-                n.pop()
-                n.push(b.value)
+            if(op.includes(b.value)){
+                if (op.includes(n[(n.length)-1])) {
+                    n.pop()
+                    n.push(b.value)
+                } else if(n.length==0){
+                    n.push(0)
+                    n.push(b.value)
+                }
+                else{
+                    n.push(b.value)
+                }
             }else{
                 n.push(b.value)
             }
-
             ans.innerHTML = n.join("")
         }
         if(b.value=="AC"){
