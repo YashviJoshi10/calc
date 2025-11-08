c5de57fc9ecc0013b825e4de76ebd511e3c9c580
Yashvi <yashvi1095@gmail.com>
2025-11-08 18:12:29 +0530

    mult & div corrected

--- script.js
+++ script.js
@@ -3,31 +3,40 @@ let ans = document.getElementById("ans")
 let n = []
 let a = 0
 let op=["+","-","/","x"]
+let op2=["+","-"]
 let left_num = ""
 let right_num = ""
 let left_idx=0
 let right_idx=0
+
+let f=false
 
 const get_nums = (o)=>{
     left_num = "";
     right_num = ""
     left_idx = n.indexOf(o)-1
     right_idx = n.indexOf(o)+1
+    f=false
+    // left no
     if (left_idx==-1) {
         left_num=0
     }
     else{
-
         while (!op.includes(n[left_idx]) && left_idx>=0) {
             left_num=n[left_idx]+left_num
             left_idx=left_idx-1
         }
     }
+
+    // right no
     if (right_idx==n.length) {
         right_num=left_num
     }
     else{
-
+        if (n[right_idx]=="-") {
+            right_idx=right_idx+1
+            f=true
+        }
         while (!op.includes(n[right_idx]) && right_idx<n.length) {
             right_num=right_num+n[right_idx]
             right_idx=right_idx+1
@@ -37,19 +46,24 @@ const get_nums = (o)=>{
     // console.log(right_num)
 }
 
-const calc = (n)=>{   
+const calc = (n)=>{
     if(n.includes("/")){
-        // console.log(n.indexOf("/"))
         get_nums("/")
         a = parseInt(left_num) / parseInt(right_num)
-        // console.log(a)
+        if (f) {
+            a=-a
+        }
         n.splice(left_idx+1,right_idx-left_idx-1,a)
-        // console.log(n)     
         calc(n)   
     }else if(n.includes("x")){
         get_nums("x")
         a = parseInt(left_num) * parseInt(right_num)
-        n.splice(left_idx+1,right_idx-left_idx-1,a)
+        // 
+        if (f) {
+            a=-a
+        }
+        n.splice(left_idx+1,right_idx-left_idx-1,a) 
+        console.log(n)       
         calc(n) 
     }else if(n.includes("-")){
         get_nums("-")
@@ -70,13 +84,10 @@ btn.forEach((b)=>{
         // console.log(b.value)
         if(b.value!="AC" && b.value!="="){            
             if(op.includes(b.value)){
-                if (op.includes(n[(n.length)-1])) {
+                if (op2.includes(n[(n.length)-1])) {
                     n.pop()
                     n.push(b.value)
-                } else if(n.length==0){
-                    n.push(0)
-                    n.push(b.value)
-                }
+                } 
                 else{
                     n.push(b.value)
                 }
@@ -90,6 +101,7 @@ btn.forEach((b)=>{
             n.length=0
         }
         if(b.value=="="){
+            console.log(n)
             calc(n)
             ans.innerHTML=a
         }
